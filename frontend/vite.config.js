aae93d72a419ec498b4f5a54a5991ef600517e34
Richian7 <richmonddadzie97@gmail.com>
2025-12-22 00:30:48 +0000

    fix: update frontend API base URL and Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// VITE_API_URL=https://nkomo-y4it.onrender.com
+
 export default defineConfig({
   plugins: [react()],
 })
